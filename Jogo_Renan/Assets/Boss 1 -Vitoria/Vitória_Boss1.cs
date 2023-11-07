b43e6215d401bed0c73cc01b853833644e1a5e7d
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-11-07 14:25:19 -0300

    Atalização

--- Jogo_Renan/Assets/Boss 1 -Vitoria/Vitória_Boss1.cs
+++ Jogo_Renan/Assets/Boss 1 -Vitoria/Vitória_Boss1.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Vitória_Boss1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public float speed;
+   ///public float walkTime;
+   private bool walkRight;
+   
+   public int health;
+   public float timer;
+   
+   private Animator anim;
+   private Rigidbody2D rig;
+   
+
+   
     void Start()
     {
+        rig = GetComponent< Rigidbody2D >();
+        anim = GetComponent< Animator >();
+
+    }
+    
+    void FixedUpdate()
+    {
+        if (walkRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        
+        if (!walkRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+    }
+    public void Damage( int dmg )
+    {
+        health -= dmg;
+        anim.SetTrigger("Hit");
+        
+        if ( health <= 0 )
+        {
+            Destroy(gameObject);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            walkRight = !walkRight;
+        }
     }
 }
