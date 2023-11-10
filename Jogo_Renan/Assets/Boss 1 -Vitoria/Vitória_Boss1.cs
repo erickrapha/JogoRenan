a0581cc0079ad69370246a9a9bd9d96e433c76f5
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-11-10 16:31:14 -0300

    Asasa
    sa

--- Jogo_Renan/Assets/Boss 1 -Vitoria/Vitória_Boss1.cs
+++ Jogo_Renan/Assets/Boss 1 -Vitoria/Vitória_Boss1.cs
@@ -1,56 +1,61 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vitória_Boss1 : MonoBehaviour
 {
-   public float speed;
-   ///public float walkTime;
-   private bool walkRight;
-   
-   public int health = 5;
-   public float timer;
-   
-   private Animator anim;
-   private Rigidbody2D rig;
-   
+    public float speed;
+    private bool walkRight;
 
-   
+    public int health = 5;
+    public float timer;
+    
+    public GameObject objetoPrefab; 
+    private Animator anim;
+    private Rigidbody2D rig;
+    public Player player;
+    public Text vida; 
+
+    public float tempoTiro;
+
     void Start()
     {
-        rig = GetComponent< Rigidbody2D >();
-        anim = GetComponent< Animator >();
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
         ControllerVitoria.instance.UpdateLives(health);
 
+        InvokeRepeating("SpawnObject", tempoTiro, tempoTiro);
     }
-    
+
     void FixedUpdate()
     {
+        UpdateHealthText();
         if (walkRight)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 180);
         }
-        
-        if (!walkRight)
+        else
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 0);
         }
-
     }
-    public void Damage( int dmg )
+
+    public void Damage(int dmg)
     {
         health -= dmg;
         anim.SetTrigger("Hit");
-        
-        if ( health <= 0 )
+
+        if (health <= 0)
         {
-            Destroy(gameObject);
+            Destroy(gameObject, 0.3f);
+            anim.SetInteger("transition", 3);
         }
-        
+
+        UpdateHealthText();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -58,6 +63,32 @@ public class Vitória_Boss1 : MonoBehaviour
         if (col.gameObject.CompareTag("Wall"))
         {
             walkRight = !walkRight;
+        }
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            player.Damage(1);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Tiro"))
+        {
+            Damage(1);
+        }
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(objetoPrefab, transform.position, Quaternion.identity);
+    }
+
+    void UpdateHealthText()
+    {
+        if (vida != null)
+        {
+            vida.text = "Vida: " + health.ToString();
         }
     }
 }
